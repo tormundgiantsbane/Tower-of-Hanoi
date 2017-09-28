976881e5dc9c7ecb44de2b02626c53e2b161d16e
Roman Fedoruk <rfedoruk21@gmail.com>
2017-09-28 09:29:36 -0400

    kinda works, but not really

--- main.js
+++ main.js
@@ -5,11 +5,60 @@ var threeTowers = [[1, 2, 3], [], []];
 
 var minMoves = Math.pow(2, towerLength) -1;
 
-threeTowers[0].splice(0, 1);
-threeTowers[1].splice(0, 0, 1);
+var even = minMoves % 2 == 0;
 
+//if(even){
+//	
+//}else if(!even){
+//	threeTowers[0].splice(0, 1);
+//	threeTowers[1].splice(0, 0, 1);
+//}
   
-for(var i=0; i<minMoves; i++){
+for(var i=0, x=1; i<1; i++){ //build in case for odd tower
+	if(x==1){
+		if(threeTowers[0][0] < threeTowers[1][0] || typeof threeTowers[1][0] === "undefined"){
+			var AtoB = threeTowers[0][0];
+			threeTowers[0].splice(0, 1);
+			threeTowers[1].unshift(AtoB);
+		}else if(threeTowers[0][0] > threeTowers[1][0]){
+//			var BtoA = threeTowers[1][0];
+//			threeTowers[1].splice(0, 1);
+//			threeTowers[0].unshift(BtoA);
+		}
+		
+		x=2;
+	}
+	
+	
+	if(x==2){
+		if(threeTowers[0][0] < threeTowers[2][0] || typeof threeTowers[2][0] === "undefined"){ // A to C
+			
+			var AtoC = threeTowers[0][0];
+			threeTowers[0].splice(0, 1);
+			threeTowers[2].unshift(AtoC);
+			
+		}else if(threeTowers[0][0] < threeTowers[2][0]){ // C to A
+			var CtoA = threeTowers[2][0];
+			threeTowers[2].splice(0, 1);
+			threeTowers[0].unshift(CtoA);
+		}
+		x=3;
+	}
+	
+	if(x==3){
+		if(threeTowers[1][0] < threeTowers[2][0]){ // B to C
+			var BtoC = threeTowers[1][0];
+			threeTowers[1].splice(0, 1);
+			threeTowers[2].unshift(BtoC);
+		} else if(threeTowers[1][0] > threeTowers[2][0]){ // C to B
+			var CtoB = threeTowers[2][0];
+			threeTowers[2].splice([0, 1]);
+			threeTowers[1].unshift(CtoB);
+		}
+		
+		x=1;
+	}
+	
 	
 }
 
